0391fecc5f744df3ca34efce225390eedf0a6e9e
fbidu <bidu.pub@gmail.com>
2015-11-03 00:34:49 -0200

    Adding basic input reading to main.c

--- C/labs/Lee/08/main.c
+++ C/labs/Lee/08/main.c
@@ -1,4 +1,18 @@
+#include<stdio.h>
+#include<stdlib.h>
+
 int main(int argc, char *argv[]) {
+	int interactions, n, max_objects_per_node, max_levels, root_width, root_height, i;
+	int object_id, object_x, object_y, object_vx, object_vy, object_radius;
+
+	scanf("%d %d", &interactions, &n);
+	scanf("%d %d %d %d", &max_objects_per_node, &max_levels, &root_width, &root_height);
+
+
+	for (i = 0; i < n; i++)
+	{
+		scanf("%d %d %d %d %d %d", &object_id, &object_x, &object_y, &object_vx, &object_vy, &object_radius);
+	}
 
     return 0;
 }
