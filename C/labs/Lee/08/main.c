efd467fda8930104be55fb26ab75ccb27d0e234e
fbidu <bidu.pub@gmail.com>
2015-11-10 23:45:59 -0200

    Formating the source for 08/main

--- C/labs/Lee/08/main.c
+++ C/labs/Lee/08/main.c
@@ -3,16 +3,21 @@
 
 int main(int argc, char *argv[])
 {
-	int interactions, n, max_objects_per_node, max_levels, root_width, root_height, i;
-	int object_id, object_x, object_y, object_vx, object_vy, object_radius;
+	int interactions, n, max_objects_per_node, max_levels,
+		root_width, root_height, i;
+
+	int object_id, object_x, object_y,
+		object_vx, object_vy, object_radius;
 
 	scanf("%d %d", &interactions, &n);
-	scanf("%d %d %d %d", &max_objects_per_node, &max_levels, &root_width, &root_height);
+	scanf("%d %d %d %d", &max_objects_per_node, &max_levels,
+						 &root_width, &root_height);
 
 
 	for (i = 0; i < n; i++)
 	{
-		scanf("%d %d %d %d %d %d", &object_id, &object_x, &object_y, &object_vx, &object_vy, &object_radius);
+		scanf("%d %d %d %d %d %d", &object_id, &object_x,
+			 &object_y, &object_vx, &object_vy, &object_radius);
 	}
 
     return 0;
