456a5111c63b9b470fc661cf9e20e1312b26395c
fbidu <bidu.pub@gmail.com>
2015-11-10 23:46:14 -0200

    Implementing the functions at entities.h

--- C/labs/Lee/08/entities.c
+++ C/labs/Lee/08/entities.c
@@ -2,9 +2,26 @@
 
 #include "entities.h"
 
-ball* entity_createBall(int id, double x, double y, double vx, double vy, double radius) {
+ball* entity_createBall(int id, double x, double y, double vx, double vy, double radius)
+{
+    ball *created = malloc(sizeof(ball));
+
+    if (created)
+    {
+        created->id = id;
+        created->x = x;
+        created->y = y;
+        created->vx = vx;
+        created->vy = vy;
+        created->radius = radius;
+
+        return created;
+    }
+
     return NULL;
 }
 
-void entity_destroyBall(ball *b) {
+void entity_destroyBall(ball *b)
+{
+    free(b);
 }
