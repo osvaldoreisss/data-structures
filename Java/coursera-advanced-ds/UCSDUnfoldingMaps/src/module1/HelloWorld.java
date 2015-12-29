5cb99bab719ddb0ded6757a934952b53acadc109
fbidu <bidu.pub@gmail.com>
2015-12-29 19:08:43 -0200

    Adding my code for the homework from lesson 1

--- Java/coursera-advanced-ds/UCSDUnfoldingMaps/src/module1/HelloWorld.java
+++ Java/coursera-advanced-ds/UCSDUnfoldingMaps/src/module1/HelloWorld.java
@@ -73,7 +73,10 @@ public class HelloWorld extends PApplet
 		
 		// This line makes the map interactive
 		MapUtils.createDefaultEventDispatcher(this, map1);
-		
+
+		map2 = new UnfoldingMap(this, 450, 50, 350, 500, provider);
+		map2.zoomAndPanTo(zoomLevel, new Location(-21.486758, -51.538767));
+		MapUtils.createDefaultEventDispatcher(this, map2);
 		// TODO: Add code here that creates map2 
 		// Then you'll modify draw() below
 
@@ -84,6 +87,7 @@ public class HelloWorld extends PApplet
 		// So far we only draw map1...
 		// TODO: Add code so that both maps are displayed
 		map1.draw();
+		map2.draw();
 	}
 
 	
